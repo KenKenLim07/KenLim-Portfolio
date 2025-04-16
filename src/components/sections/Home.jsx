9e510d9e50c47826390a5efc8bba9ab0cf320961
KenKenLim07 <josemarielim7@gmail.com>
2025-04-17 03:18:47 +0800

    ahahah

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -16,7 +16,7 @@ export const Home = ({ showTypewriter, showFadeUp, showfadein }) => {
             Hi, I'm Jose Marie Lim
           </h1>
 
-          <p className="rounded-xl p-4 border-2 border-white/20 hover:border-cyan-500 hover:-translate-y-1 transition-all mb-8 text-center bg-white/5 backdrop-blur-md">
+          <p className="rounded-xl p-8 border-2 border-white/20 hover:border-cyan-500 hover:-translate-y-1 transition-all mb-8 text-center bg-white/5 backdrop-blur-md">
             A driven tech enthusiast with a strong focus on coding,
             cybersecurity, operating system development, and web app
             development. Currently pursuing a Computer Science degree, I'm
@@ -32,14 +32,14 @@ export const Home = ({ showTypewriter, showFadeUp, showfadein }) => {
           <div className="flex flex-wrap justify-center gap-2 text-sm mt-4">
             <a
               href="#projects"
-              className="whitespace-nowrap bg-transparent text-white border-2 border-yellow-400 py-3 px-6 rounded-xl font-medium transition-all duration-300 hover:-translate-y-1 hover:border-yellow-300 hover:bg-yellow-300/20 hover:shadow-lg"
+              className="whitespace-nowrap bg-transparent text-white border-2 border-yellow-400 py-3 px-6 rounded-4xl font-medium transition-all duration-300 hover:-translate-y-1 hover:border-yellow-300 hover:bg-yellow-300/20 hover:shadow-lg"
             >
               View Projects
             </a>
 
             <a
               href="#contact"
-              className="whitespace-nowrap bg-transparent text-white border-2 border-cyan-400 py-3 px-6 rounded-xl font-medium transition-all duration-300 hover:-translate-y-1 hover:border-cyan-300 hover:bg-cyan-300/20 hover:shadow-lg"
+              className="whitespace-nowrap bg-transparent text-white border-2 border-cyan-400 py-3 px-6 rounded-4xl font-medium transition-all duration-300 hover:-translate-y-1 hover:border-cyan-300 hover:bg-cyan-300/20 hover:shadow-lg"
             >
               Contact Me
             </a>
@@ -48,7 +48,7 @@ export const Home = ({ showTypewriter, showFadeUp, showfadein }) => {
               href="https://drive.google.com/file/d/14YnWNfobuUFlAlLK7O4cdCA5Sa9OZ-qU/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
-              className="whitespace-nowrap bg-transparent text-white border-2 border-pink-400 py-3 px-6 rounded-xl font-medium transition-all duration-300 hover:-translate-y-1 hover:border-pink-300 hover:bg-pink-300/20 hover:shadow-lg"
+              className="whitespace-nowrap bg-transparent text-white border-2 border-pink-400 py-2 px-6 rounded-4xl font-medium transition-all duration-300 hover:-translate-y-1 hover:border-pink-300 hover:bg-pink-300/20 hover:shadow-lg"
             >
               Download CV
             </a>
