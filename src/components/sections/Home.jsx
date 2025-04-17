cb32673426c4f637022c9e951a8a35059e40b588
KenKenLim07 <josemarielim7@gmail.com>
2025-04-18 04:10:52 +0800

    removed border in home

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -8,23 +8,19 @@ export const Home = ({ showTypewriter, showFadeUp, showfadein }) => {
     >
       <div className="text-center z-10 px-4 max-w-xl mx-auto">
         <RevealOnScroll>
-          <h1
-            className={`font-bold mb-3 text-white leading-tight transition-all duration-300 ease-in-out text-2xl ${
-              showTypewriter ? "typewriter" : ""
-            }`}
-          >
-            Hi, I'm Jose Marie Lim
-          </h1>
+        <h1
+  className={`font-bold mb-3 text-white leading-tight transition-all duration-300 ease-in-out text-2xl ${
+    showTypewriter ? "typewriter" : ""
+  }`}
+>
+  <span className="text-xl opacity-50">Hi, I'm </span>
+  
+  <span className="text-4xl ">Jose Marie Lim</span>
+</h1>
 
-          <p className="rounded-xl p-6 border-2 border-white/20 hover:border-cyan-500 hover:-translate-y-1 transition-all mb-8 text-center">
-            A driven tech enthusiast with a strong focus on coding,
-            cybersecurity, operating system development, and web app
-            development. Currently pursuing a Computer Science degree, I'm
-            dedicated to mastering the technical skills required to excel in the
-            ever-evolving field of technology. Passionate about solving complex
-            problems, optimizing systems, and building secure and efficient
-            applications. Constantly pushing myself to innovate, learn, and
-            grow.
+
+          <p className="rounded-xl p-4 border- border-white/20 hover:border-cyan-500 hover:-translate-y-1 transition-all mb-8 text-center" >
+          Driven tech enthusiast pursuing a Computer Science degree with a focus on coding, cybersecurity, OS development, and web apps. Passionate about solving complex problems, building secure systems, and constantly pushing boundaries to innovate and grow.
           </p>
         </RevealOnScroll>
 
@@ -32,14 +28,14 @@ export const Home = ({ showTypewriter, showFadeUp, showfadein }) => {
           <div className="flex flex-wrap justify-center space-x-2 text-sm mt-4 gap-3">
             <a
               href="#projects"
-              className="whitespace-nowrap bg-transparent text-white border-3 border-yellow-400 py-2 px-5 rounded-4xl font-medium transition-all duration-300 hover:-translate-y-1 hover:border-yellow-300 hover:bg-yellow-300/20 hover:shadow-lg"
+              className="whitespace-nowrap bg-transparent text-white border-2 border-yellow-400 py-2 px-5 rounded-4xl font-medium transition-all duration-300 hover:-translate-y-1 hover:border-yellow-300 hover:bg-yellow-300/20 hover:shadow-lg"
             >
               View Projects
             </a>
 
             <a
               href="#contact"
-              className="whitespace-nowrap bg-transparent text-white border-3 border-cyan-400 py-2 px-5 rounded-4xl font-medium transition-all duration-300 hover:-translate-y-1 hover:border-cyan-300 hover:bg-cyan-300/20 hover:shadow-lg"
+              className="whitespace-nowrap bg-transparent text-white border-2 border-cyan-400 py-2 px-5 rounded-4xl font-medium transition-all duration-300 hover:-translate-y-1 hover:border-cyan-300 hover:bg-cyan-300/20 hover:shadow-lg"
             >
               Contact Me
             </a>
@@ -48,7 +44,7 @@ export const Home = ({ showTypewriter, showFadeUp, showfadein }) => {
               href="https://drive.google.com/file/d/14YnWNfobuUFlAlLK7O4cdCA5Sa9OZ-qU/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
-              className="whitespace-nowrap bg-transparent text-white border-3 border-pink-400 py-2 px-5 rounded-4xl font-medium transition-all duration-300 hover:-translate-y-1 hover:border-pink-300 hover:bg-pink-300/20 hover:shadow-lg"
+              className="whitespace-nowrap bg-transparent text-white border-2 border-pink-400 py-2 px-5 rounded-4xl font-medium transition-all duration-300 hover:-translate-y-1 hover:border-pink-300 hover:bg-pink-300/20 hover:shadow-lg"
             >
               Download CV
             </a>
